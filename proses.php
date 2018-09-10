aca3673e7fbbfbb1b22cca17d980957c51b0d7b2
melishad <42725372+melishad@users.noreply.github.com>
2018-09-10 09:56:52 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,4 +1,37 @@
 <?php 
 //Tuliskan kode anda di bawah ini
+	function bintang($value){
 
+		samping($value);
+		echo "<br>";
+		$value--;
+		if ($value >= 1) {
+			bintang($value);
+		}
+		function samping($batas, $awal=1); 
+		echo "*";
+		$awal++;
+		if ($awal <=batas) 
+			samping($batas,$awal);
+		}
+		
+
+	function bilangan ($value){
+		$ganjil = array();
+		$genap = array();
+		for( $i = 1,$i < $nilai $i ++) { 
+			if($bil%2==0){
+				array_push  ($ganjil, $i);
+			}else {
+				array_push ($genap, $i);
+			}
+		
+		echo "ganjil :";
+	}
+}
+	if (isset($_GET ['submit'])) {
+			$angka = isset ($_GET['angka']) ? $_GET['angka'] : 0;
+			bintang($angka);
+		}
+	
 ?>
